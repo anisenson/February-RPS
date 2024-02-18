8dc54c99a004bc8396af0747b252ce829ad3d119
Adam Nisenson <144464774+anisenson@users.noreply.github.com>
2024-02-18 16:18:21 +0000

    Started Javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+// Set starting score to 0
+let playerScore = 0;
+let computerScore = 0;
+
+// variables
+const playerScoreElement = document.getElementById('playerScore');
+const computerScoreElement = document.getElementById('ComputerScore');
+const outcomeElement = document.getElementById('outcome');
+
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+const resetBtn = document.getElementById('resetBtn');
