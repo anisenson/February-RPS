a12473ef8b2f587f22466f2ab57cc117376ba928
Adam Nisenson <144464774+anisenson@users.noreply.github.com>
2024-02-18 16:21:36 +0000

    created function

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ const rockBtn = document.getElementById('rockBtn');
 const paperBtn = document.getElementById('paperBtn');
 const scissorsBtn = document.getElementById('scissorsBtn');
 const resetBtn = document.getElementById('resetBtn');
+
+
+// Event Listeners (Googled)
+rockBtn.addEventListener('click', () => playGame('rock'));
+paperBtn.addEventListener('click', () => playGame('paper'));
+scissorsBtn.addEventListener('click', () => playGame('scissors'));
+resetBtn.addEventListener('click', resetGame);
+
+// function to actually play the game
+function playGame(playerChoice) {
+    const choices = ['rock', 'paper', 'scissors']; //the choices
+    const computerChoice = choices[Math.floor(Math.random() * 3)]; //math 
+
+  
